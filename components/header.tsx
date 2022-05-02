7862b77bbe9dee1e9e6fea969f71a558274cc88d
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-02 17:08:47 +0200

    add

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import styles from "./header.module.css"
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
-// rendering, and avoids any flash incorrect content on initial page load.
+// rendering, and avoids any flash incorrect content on initial page load.s
 export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
