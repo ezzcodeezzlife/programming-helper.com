35a336f48f414f3a7a28024d93121ea4c127112c
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-04 18:08:42 +0200

    fix 2

--- pages/api/login.ts
+++
@@ -1,6 +0,0 @@
-import { loginHandler } from "@storyofams/next-password-protect";
-
-export default loginHandler("YOUR_SECRET_PASSWORD", {
-  // Options go here (optional)
-  cookieName: "next-password-protect",
-});
