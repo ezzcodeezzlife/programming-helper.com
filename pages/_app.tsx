eaebc70bce6132ec1dc752469973c0d9c0c6fca9
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-04 18:06:54 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import "./styles.css"
-import { withPasswordProtect } from "@storyofams/next-password-protect";
+
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
