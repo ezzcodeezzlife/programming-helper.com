9ffe0f55674a19ec1cea02d1369ccb4356fcb7e8
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-04 22:58:35 +0200

    add p

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,6 +1,6 @@
 import { loginHandler } from "@storyofams/next-password-protect";
 
-export default loginHandler("test", {
+export default loginHandler("lemon4", {
   // Options go here (optional)
   cookieName: "next-password-protect",
 });
--- pages/api/passwordCheck.ts
+++ pages/api/passwordCheck.ts
@@ -1,6 +1,6 @@
 import { passwordCheckHandler } from "@storyofams/next-password-protect";
 
-export default passwordCheckHandler("test", {
+export default passwordCheckHandler("lemon4", {
   // Options go here (optional)
   cookieName: "next-password-protect",
 });
