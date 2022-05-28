228c3738f563734e5b2cea5f452d704cd96a7d02
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-28 13:13:46 +0200

    increase rate limit

--- pages/api/examples/generate-function.ts
+++ pages/api/examples/generate-function.ts
@@ -30,7 +30,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await limiter.check(res, 3, "CACHE_TOKEN") // 8 requests per minute
+  await limiter.check(res, 6, "CACHE_TOKEN") // 8 requests per minute
 
   const session = await getSession({ req })
 
