c887f7a2ad41f1b7054f3b3c8d2a464b3129a29d
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-31 00:54:10 +0200

    add more tailwind styling

--- components/header.tsx
+++ components/header.tsx
@@ -181,7 +181,7 @@ export default function Header() {
 
 <Popover className="relative bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="/">
               <span className="sr-only">Workflow</span>
