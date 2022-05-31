89bd2918797a645bd4254c72f9511c92bb1f5ed0
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-31 17:33:12 +0200

    add adsense code

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,30 +9,7 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <>
-      <Script
-        id="Adsense-id"
-        data-ad-client="ca-pub-8251732556629149"
-        async
-        strategy="afterInteractive"
-        onError={(e) => {
-          console.error("Script failed to load", e)
-        }}
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
-
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-WMLR3PFFXE"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-WMLR3PFFXE');
-        `}
-      </Script>
+      
 
       <Header />
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,38 @@ import type { AppProps } from "next/app"
 import { withPasswordProtect } from "@storyofams/next-password-protect"
 import "../styles/globals.css"
 import "./styles.css"
+import Script from "next/script"
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+    <>
+    <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-8251732556629149"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.error("Script failed to load", e)
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WMLR3PFFXE"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WMLR3PFFXE');
+        `}
+      </Script>
     <SessionProvider session={pageProps.session} refetchInterval={0}>
       <Component {...pageProps} />
     </SessionProvider>
+    </>
   )
 }
 
