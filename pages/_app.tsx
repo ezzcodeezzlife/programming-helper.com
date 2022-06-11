d92f5b2a23e00a5ae6b6039e9ca823f4f3c944e1
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-12 00:27:01 +0200

    debug favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,7 +46,10 @@ const App = ({ Component, pageProps }: AppProps) => {
       </Script>
 
       <Head>
-          <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
         </Head>
 
       <SessionProvider session={pageProps.session} refetchInterval={0}>
