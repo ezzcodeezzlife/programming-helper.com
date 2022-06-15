4c6e38862e582d9039b4669a5e1d81ec84a59f03
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-16 00:16:55 +0200

    change cta etxt

--- pages/index.tsx
+++ pages/index.tsx
@@ -326,7 +326,7 @@ export default function Example() {
         <div className="bg-gray-50 m-6	rounded-xl">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              <span className="block">Question about something?</span>
+              <span className="block">Question about programming?</span>
               <span className="block text-indigo-600">Chat with personal AI assistant</span>
             </h2>
             <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
