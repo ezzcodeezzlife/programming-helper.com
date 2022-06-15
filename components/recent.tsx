228c01bd198ae08b35941f05340c776b301458c5
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-15 16:35:35 +0200

    fix buidl error

--- components/recent.tsx
+++ components/recent.tsx
@@ -92,7 +92,6 @@ export default function Usuarios() {
 */}
 
 
-  */} 
 
 
 
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -192,7 +192,15 @@ export default function translate() {
       </div>
       <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"></img>
    </div>
+</div><div className="chat-message">
+   <div className="flex items-end justify-end">
+      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+         <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Your error message says permission denied, npm global installs must be given root privileges.</span></div>
+      </div>
+      <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"></img>
+   </div>
 </div>
+
 
 
 
