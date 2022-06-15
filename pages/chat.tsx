bb6c86eaf466d5bacbe4e5f7b03943e65a55076e
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-15 15:00:19 +0200

    (test) rm chat text msgs

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -79,74 +79,8 @@ export default function translate() {
       <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"></img>
    </div>
 </div>
-<div className="chat-message">
-   <div className="flex items-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">Command was run with root privileges. I'm sure about that.</span></div>
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I've update the description so it's more obviously now</span></div>
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">FYI https://askubuntu.com/a/700266/510172</span></div>
-         <div>
-            <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
-               Check the line above (it ends with a # so, I'm running it as root )
-               <pre># npm install -g @vue/devtools</pre>
-            </span>
-         </div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1"></img>
-   </div>
-</div>
-<div className="chat-message">
-   <div className="flex items-end justify-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-         <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Any updates on this issue? I'm getting the same error when trying to install devtools. Thanks</span></div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"></img>
-   </div>
-</div>
-<div className="chat-message">
-   <div className="flex items-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-         <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Thanks for your message David. I thought I'm alone with this issue. Please, ? the issue to support it :)</span></div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1"></img>
-   </div>
-</div>
-<div className="chat-message">
-   <div className="flex items-end justify-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-blue-600 text-white ">Are you using sudo?</span></div>
-         <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Run this command sudo chown -R `whoami` /Users/asdasd/.npm-global/ then install the package globally without using sudo</span></div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"></img>
-   </div>
-</div>
-<div className="chat-message">
-   <div className="flex items-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">It seems like you are from Mac OS world. There is no /Users/ folder on linux ?</span></div>
-         <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">I have no issue with any other packages installed with root permission globally.</span></div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1"></img>
-   </div>
-</div>
-<div className="chat-message">
-   <div className="flex items-end justify-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-         <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">yes, I have a mac. I never had issues with root permission as well, but this helped me to solve the problem</span></div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2"></img>
-   </div>
-</div>
-<div className="chat-message">
-   <div className="flex items-end">
-      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I get the same error on Arch Linux (also with sudo)</span></div>
-         <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I also have this issue, Here is what I was doing until now: #1076</span></div>
-         <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">even i am facing</span></div>
-      </div>
-      <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1"></img>
-   </div>
-</div>
+
+
 </div>
 <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
 <div className="relative flex">
