ba36e3593b951f2a85e7d8fff5b4ff2b27023581
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-06-15 22:13:47 +0200

    change cta to chat ^ add padding to chat

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -103,7 +103,7 @@ export default function translate() {
     <>
       {/*@ts-ignore*/}
       <center>
-        <div className=" flex-1   justify-between flex flex-col h-screen xl:w-2/3 2xl:w-1/3">
+        <div className=" flex-1 pb-16  justify-between flex flex-col h-screen xl:w-2/3 2xl:w-1/3">
           <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
             <div className="relative flex items-center space-x-4 pl-4">
               <div className="relative">
@@ -193,91 +193,37 @@ export default function translate() {
             ))}
           </div>
           <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-            <div className="relative flex">
+            <div className="relative ">
+            <center>
               <form className="" onSubmit={handleSubmit}>
                 <input
+                className=" bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   type="text"
                   value={input}
                   onChange={(event) => setInput(event.target.value)}
-                  placeholder="Write your message!"
+                  placeholder="Write your message! ..."
                   className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
                 />
+                <span></span>
+
+                
                 <input 
                   type="text"
+                  className="hidden"
                   id="first"
                   name="first"
                   value=""
                   onChange={(event) => setInput(event.target.value)}
                   autoComplete="off"
                 />
+              
                 <button className="hidden" type="submit">
                   Submit
                 </button>
               </form>
-              <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
-                <button
-                  type="button"
-                  className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="h-6 w-6 text-gray-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                    ></path>
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="h-6 w-6 text-gray-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-                    ></path>
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-                    ></path>
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="h-6 w-6 text-gray-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                  </svg>
-                </button>
+              </center>
+              <div className="absolute right-0 items-center inset-y-0  sm:flex">
+               
                 <button
                   onClick={handleSubmit}
                   type="button"
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import Seocomponent from "../components/seocomponent"
 
 const navigation = [
   { name: "Features", href: "#features" },
-  { name: "Application", href: "/generate-function" },
+  { name: "Chat", href: "/chat" },
   { name: "GitHub", href: "https://github.com/ezzcodeezzlife/openaiwebsite" },
   {
     name: "Privacy Policy",
@@ -322,26 +322,26 @@ export default function Example() {
         </div>
       </div>
 
-      <div className="p-12	">
-        <div className="bg-gray-50">
+     
+        <div className="bg-gray-50 m-6	rounded-xl">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              <span className="block">Ready to dive in?</span>
-              <span className="block text-indigo-600">Try it out now.</span>
+              <span className="block">Question about something?</span>
+              <span className="block text-indigo-600">Chat with personal AI assistant</span>
             </h2>
             <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
               <div className="inline-flex rounded-md shadow">
                 <a
-                  href="generate-function"
+                  href="/chat"
                   className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                 >
-                  Get started
+                  Get chatting
                 </a>
               </div>
             </div>
           </div>
         </div>
-      </div>
+  
     </>
   )
 }
