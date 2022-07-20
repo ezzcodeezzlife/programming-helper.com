a0616d08d8f31fd49fb04eb3c0e3f7a7277d8d26
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-07-20 02:05:14 +0200

    add padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -323,7 +323,7 @@ export default function Example() {
         </div>
       </div>
 
-      <div className="bg-gray-50 m-6	rounded-xl">
+      <div className="bg-gray-50 m-6 my-14	rounded-xl">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Question about programming?</span>
