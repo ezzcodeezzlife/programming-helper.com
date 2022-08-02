f3a1af491e710e66d3edc1af91ce5c482cb4c528
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-08-02 20:17:46 +0200

    test adsense code

--- pages/index.tsx
+++ pages/index.tsx
@@ -324,6 +324,7 @@ export default function Example() {
         </div>
       </div>
 
+<div id="test" style={{padding: 20, backgroundColor: "yellow"}}>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           
@@ -341,6 +342,7 @@ export default function Example() {
  </script>
         `}
       </Script>
+    </div>
 
       <div className="bg-gray-50 m-6 my-14	rounded-xl">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
