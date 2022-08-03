3b6eba69f9bf1eb72bcb428db5f74f83b3d9e7f3
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-08-03 20:14:23 +0200

    update test

--- pages/api/examples/tests.ts
+++ pages/api/examples/tests.ts
@@ -3,6 +3,19 @@ import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from "next"
 import rateLimit from "../../../utils/rate-limit"
 import { env } from "process"
+import { MongoClient } from "mongodb"
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}
+const client = new MongoClient(process.env.MONGO_URI!)
+
+interface Userpromt {
+  input: string
+  output: string
+  createdAt: string
+}
 
 const limiter = rateLimit({
   interval: 60 * 1000, // 60 seconds
@@ -11,19 +24,21 @@ const limiter = rateLimit({
 
 const { Configuration, OpenAIApi } = require("openai")
 
-const configuration = new Configuration({
-  apiKey: env.OPENAI_API_KEY,
-})
-const openai = new OpenAIApi(configuration)
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await limiter.check(res, 4, "CACHE_TOKEN") // 8 requests per minute
+  await limiter.check(res, 20, "CACHE_TOKEN") // 20 requests per minute
+
+  let configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+  let openai = new OpenAIApi(configuration)
 
   const session = await getSession({ req })
 
   //console.log(req.body)
   //console.log(req.body.textup)
   //console.log(req.body.selectedOption.value)
+
+  console.log(session)
 
   console.log("content length", req.body.textup.length)
   if (req.body.textup.length > 1000) {
@@ -37,8 +52,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { user } = session
 
     openai
-      .createCompletion("content-filter-alpha", {
-        //text-davinci-002,
+      .createCompletion({
+        model: "content-filter-alpha",
         prompt: "<|endoftext|>" + req.body.textup + "\n--\nLabel:",
         temperature: 0,
         max_tokens: 1,
@@ -52,24 +67,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
           console.log("usermail:", user?.email)
 
-          // add sending user id to the request
+          configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY_CODEX,
+          })
+          openai = new OpenAIApi(configuration)
 
           openai
-            .createCompletion("text-davinci-002", {
+            .createCompletion({
+              model: "code-davinci-002",
               prompt:
-                "A unit test for the following Code: " +
+                "Test for this code: " +
                 req.body.textup +
-                "\n\n\n Unit Test:\n\n\n\n",
-              temperature: 0.7,
+                " \n \n Test:\n\n",
+              temperature: 0.5,
               max_tokens: 250,
               top_p: 1,
-              frequency_penalty: 0,
+              frequency_penalty: 0.3,
               presence_penalty: 0,
               user: user?.email,
             })
-            .then((response: any) => {
+            .then(async (response: any) => {
               console.log(response.data.choices[0].text)
               //res.status(200).json(response.data)
+              console.log("Response:", response.data.choices[0])
               try {
                 res.status(200).json({ data: response.data.choices[0].text })
               } catch (err) {
